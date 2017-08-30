762d99406a586603f2ceb7ac6cfeb552c0abb746
Patrick Panahon <patrick.panahon@gmail.com>
2017-08-30 16:27:21 -0400

    initial commit, youtube and lyrics features functioning

---
+++ index.js
@@ -0,0 +1,65 @@
+const lyricsAPI = "https://api.lyrics.ovh/v1";
+const youtubeAPI = "https://www.googleapis.com/youtube/v3/search";
+
+function handleSearchForm() {
+  $('.search-form').submit(event => {
+    event.preventDefault();
+    const artistQuery = $('.artist-string').val();
+    const songQuery = $('.song-string').val();
+    const combinedQuery = `${artistQuery} ${songQuery}`;
+    clearInputFields();
+    unhideContainers();
+    getYoutubeData(combinedQuery, displayYoutubeResults);
+    getLyricsData(artistQuery, songQuery, displayLyricsResults);
+  });
+}
+
+function unhideContainers() {
+  $('.container').prop('hidden', false);
+}
+
+function clearInputFields() {
+  $('artist.string').val("");
+  $('song.string').val("");
+}
+
+function getLyricsData(artistQuery, songQuery, callback) {
+  $.getJSON(`${lyricsAPI}/${artistQuery}/${songQuery}`, callback);
+}
+
+function displayLyricsResults(data) {
+  const lyricsData = data.lyrics.replace(/\n/gi,"<br>");
+  console.log(lyricsData);
+  $('.lyrics').html(lyricsData);
+}
+
+function getYoutubeData(searchQuery, callback) {
+  const query = {
+    part: 'snippet',
+    q: `artist ${searchQuery}`,
+    type: 'video',
+    maxResults: 3,
+    key: 'AIzaSyAUuE2ybmwEb08dCMkOv6HvW1gJDi8mhbY',
+    videoCategoryId: '10'
+  };
+  $.getJSON(youtubeAPI, query, callback);
+}
+
+function displayYoutubeResults(data) {
+  const resultsData = data.items.map((item) => renderYoutubeResults(item));
+  $('.youtube').html(resultsData);
+}
+
+function renderYoutubeResults(result) {
+  return `
+      <div class="youtube-single-result">
+        <a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">
+          <img class="youtube-thumbnail" src="${result.snippet.thumbnails.high.url}">
+          <h3 class="youtube-description">${result.snippet.title}</h3>
+        </a>
+        <p>${result.snippet.description}</p>
+      </div>
+  `;
+}
+
+handleSearchForm();
